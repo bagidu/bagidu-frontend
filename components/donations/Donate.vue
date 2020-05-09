de5a8df13d56276930ac4bf55de778345468cd8c
Sucipto <sucipto@pm.me>
2020-05-09 20:40:05 +0700

    Add donation page

--- components/donations/Donate.vue
+++ components/donations/Donate.vue
@@ -3,11 +3,11 @@
     <div id="container" class="md:w-1/2 lg:w-1/3 m-auto bg-white">
       <!-- Title -->
       <div id="donate-title" class="text-lg font-bold text-gray-700 text-center pt-5 pb-3 text-4xl">
-        Donasi Ke <span class="text-green-500">{{ username }}</span>
+        Donasi ke <span class="text-green-500">{{ username }}</span>
       </div>
       <!-- Amount -->
       <div id="amount" class="px-4 pb-4">
-        <div class="flex flex-row border-2 border-green-400 rounded items-center bg-white text-gray-700">
+        <div class="flex flex-row border border-green-400 rounded items-center bg-white text-gray-700">
           <div class="bg-white rounded-l px-2  font-bold text-3xl">
             Rp.
           </div>
@@ -23,7 +23,7 @@
           <div
             v-for="(item,idx) in amountPreset"
             :key="item.value"
-            class="amount-preset-item px-3 py-2 rounded border-2 border-green-400 bg-white flex-grow text-center text-xl text-gray-700 font-semibold cursor-pointer select-none"
+            class="amount-preset-item px-3 py-2 rounded border border-green-400 bg-white flex-grow text-center text-xl text-gray-700 font-semibold cursor-pointer select-none"
             :class="{
               ' mr-1':idx + 1 !== amountPreset.length,
               'bg-green-200': item.value === amount
@@ -40,7 +40,7 @@
           v-model="name"
           placeholder="Nama kamu"
           type="text"
-          class="flex-grow rounded border-2 border-green-400 px-3 py-1 w-full outline-none  text-lg font-normal"
+          class="flex-grow rounded border border-green-400 px-3 py-1 w-full outline-none  text-lg font-normal"
         >
       </div>
       <!-- Message -->
@@ -48,7 +48,7 @@
         <textarea
           v-model="message"
           placeholder="Pesan kamu"
-          class="flex-grow rounded border-2 border-green-400 px-3 py-1 w-full outline-none  text-xl font-normal text-gray-700"
+          class="flex-grow rounded border border-green-400 px-3 py-1 w-full outline-none  text-xl font-normal text-gray-700"
           rows="2"
         />
       </div>
--- pages/pay/_id.vue
+++ pages/pay/_id.vue
@@ -1,16 +1,55 @@
 <template>
   <div id="donate" class="bg-white h-screen">
-    <div id="container" class="md:w-1/2 lg:w-1/3 m-auto bg-white">
-      <QRCode :options="{width: 300}" value="00020101021226690017COM.TELKOMSEL.WWW011893600911002414220002152003260414220010303UME51450015ID.OR.GPNQR.WWW02150000000000000000303UME520454995802ID5920Placeholder merchant6007Jakarta610612345662380115Wxs7urG4Yczndkl0715Wxs7urG4Yczndkl5303360540415006304FBC9" />
+    <div id="container" class="md:w-1/4  m-auto bg-white flex flex-col items-center justify-center">
+      <div id="qr" class="flex items-center justify-center flex-col">
+        <img v-if="qrURL" :src="qrURL" alt="">
+        <img src="@/assets/images/qris.png" alt="qris logo" class="w-20 ">
+        <div id="description" class="text-xs">
+          Bayar menggunakan eWallet yang mendukung QRIS seperti GoPay,OVO,LinkAja,Dana
+        </div>
+      </div>
+      <a
+        class="px-3 py-1 bg-blue-400 rounded text-white w-full text-center"
+        :href="qrURL"
+        :download="`qris-${$route.params.id}`"
+      >Download</a>
     </div>
   </div>
 </template>
 
 <script>
-import QRCode from '@chenfengyuan/vue-qrcode'
+/* eslint-disable no-console */
+// import QRCode from '@chenfengyuan/vue-qrcode'
+import QR from 'qrcode/build/qrcode'
 export default {
   components: {
-    QRCode
+    // QRCode
+  },
+  data () {
+    return {
+      qrURL: null
+    }
+  },
+  mounted () {
+    this.getQrBlobURL()
+  },
+  methods: {
+    async getQrBlobURL () {
+      const data = '00020101021226690017COM.TELKOMSEL.WWW011893600911002414220002152003260414220010303UME51450015ID.OR.GPNQR.WWW02150000000000000000303UME520454995802ID5920Placeholder merchant6007Jakarta610612345662380115Wxs7urG4Yczndkl0715Wxs7urG4Yczndkl5303360540415006304FBC9'
+      const url = await QR.toDataURL(data, { width: 300, height: 300 })
+
+      const byteString = atob(url.split(',')[1])
+      const ab = new ArrayBuffer(byteString.length)
+      const ia = new Uint8Array(ab)
+
+      for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i)
+      }
+      const blob = new Blob([ab], { type: 'image/jpeg' })
+      const blbobURL = URL.createObjectURL(blob)
+      this.qrURL = blbobURL
+      console.log('blob url', blbobURL)
+    }
   }
 }
 </script>
