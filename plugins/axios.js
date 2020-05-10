8e659c9f28065b45d78e069d153ddf2019a89cd6
Sucipto <sucipto@pm.me>
2020-05-10 22:44:15 +0700

    hardocded backend api

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,7 +3,7 @@ export default function ({ $axios }, inject) {
   const api = $axios.create({
   })
 
-  api.setBaseURL(process.env.API_URL || 'http://localhost:3333')
+  api.setBaseURL(process.env.API_URL || 'https://donasi-app.herokuapp.com/')
 
   inject('api', api)
 }
