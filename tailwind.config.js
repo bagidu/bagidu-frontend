124feb7e1c404c20ba32371092a8832c5198358c
Sucipto <sucipto@pm.me>
2020-05-18 22:47:36 +0700

    Update purge option

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@
 */
 module.exports = {
   purge: {
-    enabled: process.env.NODE_ENV === 'production'
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
   },
   theme: {},
   variants: {
