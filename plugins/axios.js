a7f981a9939b3df7ce2c9cb684ba9d58894ecc96
Sucipto <sucipto@pm.me>
2020-05-18 13:09:02 +0700

    Change default api url

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,7 +3,7 @@ export default function ({ $axios }, inject) {
   const api = $axios.create({
   })
 
-  api.setBaseURL(process.env.API_URL || 'https://donasi-app.herokuapp.com/')
+  api.setBaseURL(process.env.API_URL || 'https://api.bagidu.id')
 
   inject('api', api)
 }
