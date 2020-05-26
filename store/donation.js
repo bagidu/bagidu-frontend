ed0e02555a75522943c9a3bb017a7abc323dde44
Sucipto <sucipto@pm.me>
2020-05-26 11:16:34 +0700

    Get donatin list from API & use timeago

--- store/donation.js
+++ store/donation.js
@@ -1,13 +1,21 @@
 export const state = () => ({
-  data: null
+  data: null,
+  transactions: []
 })
 
 export const mutations = {
   set (state, donation) {
     state.data = donation
+  },
+  setTransaction (state, transactions) {
+    state.transactions = transactions
   }
 }
 
 export const actions = {
-
+  getTransaction ({ commit }) {
+    this.$api.$get('/donation')
+      .then(transactions => commit('setTransaction', transactions))
+      .catch(err => console.log('error when fetch transaction', err))
+  }
 }
