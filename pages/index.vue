3b65c5f73a56af045b055bbdbe7a7aa93ea79342
Sucipto <sucipto@pm.me>
2020-05-28 23:13:04 +0700

    Update landing page image

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="index" class="h-screen">
-    <div id="hero" class="w-10/12 m-auto h-full">
+  <div id="index" class="hx-screen">
+    <div id="hero" class="w-10/12 m-auto ">
       <div id="hero-container" class="flex">
         <div id="left" class="flex-1 flex items-start justify-start mt-32 flex-col">
           <h2 class="text-4xl font-bold">
             Platform Dukungan Kreator
           </h2>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore veritatis laboriosam sint enim nisi hic laudantium facere sapiente magnam. Molestias officia eum omnis voluptates obcaecati dolorum aut laudantium, quaerat deleniti.</p>
+          <p><strong>Bagidu ID</strong> adalah platform penghubung <strong class="text-green-400">Kreator Digital</strong> dengan pendukung. Bergabunglah dengan komunitas kreator di seluruh Indonesia.</p>
 
           <div id="link-group" class="my-8">
             <nuxt-link to="/register" class="px-10 py-3 rounded border font-normal border-green-500 bg-green-500 text-white uppercase ">
@@ -17,8 +17,8 @@
             </nuxt-link>
           </div>
         </div>
-        <div id="right" class="flex-1">
-          <LandingSVG class="w-full" />
+        <div id="right" class="flex-1 pt-8">
+          <LandingSVG class="w-full h-auto" />
         </div>
       </div>
     </div>
