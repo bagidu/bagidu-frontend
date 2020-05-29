1158f3f68daa39b2be136b6d98d82234218bf308
Sucipto <sucipto@pm.me>
2020-05-30 00:12:35 +0700

    Add beta notice

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="#dashboard">
     <header-dashboard />
+    <p id="beta" class="text-sm bg-yellow-100 text-yellow-600 w-10/12 m-auto p-2 mt-2 rounded border border-yellow-500 text-center">
+      <strong>Bagidu ID</strong> masih dalam tahap <strong>Beta</strong>, beberapa fitur masih dalam pengerjaan, mohon bersabar 🤗
+    </p>
     <div id="content" class="w-10/12 m-auto">
       <nuxt-child v-if="user.profile" />
       <div v-else id="dashboard-loading">
