6edb46c92dc25135b276bb06ab417cb295e2f065
Sucipto <sucipto@pm.me>
2020-06-24 22:48:45 +0700

    Hide some non functional text

--- components/Header.vue
+++ components/Header.vue
@@ -1,46 +1,46 @@
 <template>
   <header class="pt-6 border-b border-gray-300">
-    <div class="w-10/12 m-auto flex items-center">
+    <div class="flex items-center w-10/12 m-auto">
       <div id="title" class="text-xl font-semibold text-gray-800">
         <nuxt-link to="/">
           Bagidu <span class="text-green-500">ID</span>
         </nuxt-link>
       </div>
-      <div class="spacer flex-grow" />
+      <div class="flex-grow spacer" />
       <div id="navbar-links" class="flex items-center">
-        <nuxt-link to="/feedback" class="text-gray-600 mr-2 border border-gray-400 rounded px-3 py-1 text-sm">
+        <nuxt-link v-if="false" to="/feedback" class="px-3 py-1 mr-2 text-sm text-gray-600 border border-gray-400 rounded">
           Feedback
         </nuxt-link>
-        <nuxt-link to="/guide" class="text-gray-600 text-sm">
+        <nuxt-link v-if="false" to="/guide" class="text-sm text-gray-600">
           Panduan
         </nuxt-link>
         <div
           id="profile"
-          class="flex items-center cursor-pointer relative select-none"
+          class="relative flex items-center cursor-pointer select-none"
           @click="onProfileOpen"
         >
           <div
             id="avatar"
-            class="ml-4 h-8 w-8 rounded-full text-center font-bold uppercase text-white flex items-center justify-center"
+            class="flex items-center justify-center w-8 h-8 ml-4 font-bold text-center text-white uppercase rounded-full"
             :class="{'bg-green-400':user && user.name, 'bg-gray-200': !user}"
           >
             {{ user && user.name ? user.name[0]:'' }}
           </div>
-          <div v-if="user" to="/dashboard" class="pl-2 py-2 text-gray-700 capitalize text-sm font-semibold">
+          <div v-if="user" to="/dashboard" class="py-2 pl-2 text-sm font-semibold text-gray-700 capitalize">
             {{ user.name }}
           </div>
-          <div v-else id="user-loading" velse class="py-2 pl-2 bg-gray-200 rounded-sm w-20 ml-2">
+          <div v-else id="user-loading" velse class="w-20 py-2 pl-2 ml-2 bg-gray-200 rounded-sm">
             <!-- Loading -->
           </div>
           <!-- Profile Options -->
           <div
             v-if="profileOpen"
             id="profile-options"
-            class="absolute bg-white shadow border border-gray-300 py-2 rounded text-gray-700"
+            class="absolute py-2 text-gray-700 bg-white border border-gray-300 rounded shadow"
             style="top: 45px; right:0; width: 170px;"
           >
             <ul class="text-sm font-semibold">
-              <li class="px-3 py-2 hover:bg-gray-200">
+              <li v-if="false" class="px-3 py-2 hover:bg-gray-200">
                 Pengautran Akun
               </li>
               <li
@@ -54,10 +54,10 @@
         </div>
       </div>
     </div>
-    <div id="top-menu" class="w-10/12 m-auto py-3 text-gray-700">
+    <div id="top-menu" class="w-10/12 py-3 m-auto text-gray-700">
       <nuxt-link
         to="/dashboard"
-        class="px py-3 uppercase text-xs font-bold"
+        class="py-3 text-xs font-bold uppercase px"
         :class="{
           'border-b-4 border-green-500 text-green-500':$route.path==='/dashboard'
         }"
@@ -66,7 +66,7 @@
       </nuxt-link>
       <nuxt-link
         to="/dashboard/streaming"
-        class="px py-3 uppercase text-xs font-bold ml-4"
+        class="py-3 ml-4 text-xs font-bold uppercase px"
         :class="{
           'border-b-4 border-green-500 text-green-500':$route.path==='/dashboard/streaming'
         }"
